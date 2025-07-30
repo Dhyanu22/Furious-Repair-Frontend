88d69d220db49dc8f00fe158f774531a54041f8e
Dhyanu22 <dhyanufrenjiya@gmail.com>
2025-07-31 02:05:33 +0530

    hmm

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: true, // Allow access from external IPs
-    allowedHosts: [".ngrok-free.app"], // Allow ngrok subdomains
+    historyApiFallback: true,
   },
 });
